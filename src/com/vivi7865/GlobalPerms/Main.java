33d5f5407aca27ecfc68373eb6dac46e62ba3d1d
vivi7865 <vivilaf@me.com>
2013-10-29 22:25:10 +0100

    Initial commit

---
+++ src/com/vivi7865/GlobalPerms/Main.java
@@ -0,0 +1,63 @@
+package com.vivi7865.GlobalPerms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	String url = "jdbc:mysql://mysql2.alwaysdata.com:3306/bobyland_minecraft";
+	String user = "bobyland_mc";
+	String password = "J?+dQ3*[9cVhyY37w2f[$=xX952Gt_MW";
+	Connection connexion = null;
+	Statement statement;
+
+
+	public void onDisable() {
+
+			
+
+	}
+
+	@Override
+	public void onEnable() {
+
+		try {
+		    Class.forName("com.mysql.jdbc.Driver");
+		} catch ( ClassNotFoundException e ) {
+		    e.printStackTrace();
+		}
+		
+		try {
+		    connexion = DriverManager.getConnection(url, user, password);
+		    statement = connexion.createStatement();
+		    
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		}
+
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+
+	public void onPJ(PlayerJoinEvent e) {
+
+		
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+			
+
+		return true;
+		
+	}
+
+}
